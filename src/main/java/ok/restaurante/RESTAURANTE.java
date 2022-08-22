33949ccebf0f708838c1820d926e390ce3d75d1c
Luis Fernando Chumbes Ramos <luisfernando3chr@gmail.com>
2022-08-21 22:03:38 -0500

    primer commit

---
+++ src/main/java/ok/restaurante/RESTAURANTE.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package ok.restaurante;
+
+
+public class RESTAURANTE {
+
+    public static void main(String[] args) {
+        I_principal Ventana1 = new I_principal();
+        Ventana1.setVisible(true);
+    }
+}
